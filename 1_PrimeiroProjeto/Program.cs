624341e8d7de969f69598becbd57676106c65b9a
Ailton Xavier <ailton.xavier@hotmail.com.br>
2022-02-17 04:19:25 -0300

    Adicionar arquivos de projeto.

---
+++ 1_PrimeiroProjeto/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class _1_PrimeiroProjeto {
+    static void Main(string[] args){
+        Console.WriteLine("Olá mundo. Projeto no Visual Studio!");
+
+        Console.ReadLine();
+    }
+
+}
